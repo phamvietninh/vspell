63a4d78eaf334eda41e70f26c76e503d150e4422
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2004-03-16 16:17:58 +0000

    add get_std_syllable test

---
+++ tests/std-syllable.cpp
@@ -0,0 +1,16 @@
+#include <syllable.h>
+#include <iostream>
+#include <string>
+#include <wordnode.h>
+
+using namespace std;
+
+int main(int argc,char **argv)
+{
+  dic_init(new WordNode(sarch["<root>"]));
+  string s;
+  while (cin >> s) {
+    cout << get_std_syllable(s) << endl;
+  }
+  return 0;
+}

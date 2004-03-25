baaa61c3035ade284b7741b9c3ef39179457af6b
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2004-03-25 23:41:34 +0000

    all-words-test with the argument "dot" will output in dot language. The argument "nofuzzy" will use WordNode instead of FuzzyWordNode

--- tests/all-words-test.cpp
+++ tests/all-words-test.cpp
@@ -3,13 +3,23 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+#include <set>
 
 using namespace std;
 
 int main(int argc,char **argv)
 {
   WFST wfst;
-  dic_init(argc > 1 ? new WordNode(sarch["<root>"]) : new FuzzyWordNode(sarch["<root>"]));
+  bool fuzzy = true;
+  bool dot = false;
+  int i,n;
+
+  for (i = 1;i < argc;i ++) {
+   if (!strcmp(argv[i],"nofuzzy")) fuzzy = false;
+   if (!strcmp(argv[i],"dot")) dot = true;
+  }
+
+  dic_init(fuzzy ? new FuzzyWordNode(sarch["<root>"]) : new WordNode(sarch["<root>"]));
 
   cerr << "Loading... ";
   get_root()->load("wordlist.wl");
@@ -27,9 +37,52 @@ int main(int argc,char **argv)
     st.standardize();
     st.tokenize();
     Words words,w2;
-    words.construct(st);
-    w2.based_on(words);
-    cout << w2;
+    w2.construct(st);
+    //w2.based_on(words);
+    if (!dot)
+      cout << w2;
+    else {
+      WordEntries &wes = *w2.we;
+      n = wes.size();
+      cout << "digraph wordlattice {" << endl;
+      cout << "\trankdir=LR;" << endl;
+      cout << "\thead;" << endl;
+      cout << "\ttail;" << endl;
+      //set<strid> nodes;
+      for (i = 0;i < n;i ++) {
+	//if (nodes.find(wes[i].node.node->get_id()) == nodes.end()) {
+	//nodes.insert(wes[i].node.node->get_id());
+	  cout << "\tn" << i << " [label=\"";
+	  std::vector<strid> syll;
+	  wes[i].node.node->get_syllables(syll);
+	  for (std::vector<strid>::size_type ii = 0;ii < syll.size();ii ++) {
+	    if (i)
+	      cout << " ";
+	    cout << sarch[syll[ii]];
+	  }
+	  cout << "\"];" << endl;
+	  //}
+      }
+
+      int ii,nn;
+      for (i = 0;i < n;i ++) {
+	WordEntry &we = wes[i];
+
+	if (we.pos == 0)
+	  cout << "\thead -> n" << we.id << ";" << endl;
+	if (we.pos+we.len => w2.get_word_count())
+	  cout << "\tn" << we.id << " -> tail;" << endl;
+	else {
+	  const WordEntryRefs &wers = w2.get_we(we.pos+we.len);
+	  nn = wers.size();
+	  for (ii = 0;ii < nn; ii ++) {
+	    cout << "\tn" << we.id << " -> n" << wers[ii]->id << ";" << endl;
+	  }
+	}
+      }
+
+      cout << "}" << endl;
+    }
     sarch.clear_rest();
   }
     
--- tests/tokenize-test.cpp
+++ tests/tokenize-test.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <spell.h>
+#include <dictionary.h>
 
 using namespace std;
 
@@ -9,13 +11,21 @@ int main()
 {
   string str;
 
+  dic_init(new WordNode(sarch["<root>"]));
+
   while (getline(cin,str)) {
+    /*
     Tokens output;
     tokenize(str,output);
     //copy(output.begin(),output.end(),ostream_iterator<string>(cout,"\n"));
     for (int i = 0;i < output.size();i ++)
       if (output[i].is_token)
 	cout << output[i].value << endl;
+    */
+    Sentence st(str);
+    st.standardize();
+    st.tokenize();
+    cout << st << endl;
   }
   return 0;
 }

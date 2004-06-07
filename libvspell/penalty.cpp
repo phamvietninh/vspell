575bc328a4087b2670b9fc87e4c9832ea10d3661
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2004-06-07 10:35:44 +0000

    fixed a stupid bug in PenaltyDAG.

--- libvspell/penalty.cpp
+++ libvspell/penalty.cpp
@@ -14,7 +14,7 @@ float PenaltyDAG::edge_value(uint node_from,uint node_to) const
 		for (uint i = 0;i < we->len;i ++)
 			if (we->fuzid & (1 << i))
 				count ++;
-		value -= (float)count*weight;
+		value -= (float)count*weight*value;
 	}
 	return value > 0 ? value : 0;
 }

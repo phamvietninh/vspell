118f2f6968ed6ade6cacc1a35f7e817b7566b789
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2006-12-01 09:50:27 +0700

    Preserve \n in std-syllable output
    It greatly helps preparing the wordlist

--- utils/std-syllable.cpp
+++ utils/std-syllable.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <wordnode.h>
+#include <sstream>
 
 using namespace std;
 
@@ -9,8 +10,18 @@ int main(int argc,char **argv)
 {
   dic_init();
   string s;
-  while (cin >> s) {
-    cout << get_std_syllable(s) << endl;
+  while (getline(cin,s)) {
+    istringstream iss(s);
+    string ss;
+    bool first = true;
+    while (iss >> ss) {
+      if (first)
+        first = false;
+      else
+        cout << " ";
+      cout << get_std_syllable(ss);
+    }
+    cout << endl;
   }
   return 0;
 }

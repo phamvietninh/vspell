f1692f758f58f7e795984fca813ab8b898a228e2
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2007-02-21 16:02:23 +0700

    Prevent overflows in sc2wngram
    <s> <digit>, <s> <opaque> and <s> <punct> may well run over
    int limit (about 2G). So long long int is used

--- utils/sc2wngram.c
+++ utils/sc2wngram.c
@@ -8,7 +8,7 @@ int main(int argc,char **argv)
 	double count,total_count;
 	total_count = 0;
 	int n_digit = 2;
-	int base ;
+	long long int base;
 
 	if (argc == 2)
 		sscanf(argv[1],"%d",&n_digit);
@@ -19,13 +19,18 @@ int main(int argc,char **argv)
 			total_count += count;
 		else {
 			if (total_count > 0)
-				printf("%s %s %d\n",ow1, ow2, (int)(total_count*base) > 0 ? (int)(total_count*base) : 1);
+				printf("%s %s %Ld\n",ow1, ow2, (long long int)(total_count*base) > 0 ? (long long int)(total_count*base) : (long long int)1);
 			strcpy(ow1,w1);
 			strcpy(ow2,w2);
 			total_count = count;
 		}
 	}
-	if ((int)(total_count*base) > 0)
-		printf("%s %s %d\n",ow1, ow2, (int)(total_count*base) > 0 ? (int)(total_count*base) : 1);
+
+	/*
+	  Force to long long int because <s> <digit>/<opaque>/<punct> 
+	  can easily run over int limit
+	*/
+	if ((long long int)(total_count*base) > 0)
+		printf("%s %s %Ld\n",ow1, ow2, (long long int)(total_count*base) > 0 ? (long long int)(total_count*base) : (long long int)1);
 	return 0;
 }
